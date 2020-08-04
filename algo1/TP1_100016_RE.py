bc31fdf742d50e50dea1fffa0c1b82e8f27080ef
Eliana Gamarra <elianagam2@gmail.com>
2020-08-03 22:03:15 -0300

    refactor

--- algo1/TP1_100016_RE.py
+++ algo1/TP1_100016_RE.py
@@ -1,8 +1,10 @@
-NIVEL = [[[1,1,0,1,1],[1,0,1,0,1],[0,1,1,1,0],[1,0,1,0,1],[1,1,0,1,1]],
-[[0,1,0,1,0],[1,1,0,1,1],[0,1,0,1,0],[1,0,1,0,1],[1,0,1,0,1]],
-[[1,0,0,0,1],[1,1,0,1,1],[0,0,1,0,0],[1,0,1,0,0],[1,0,1,1,0]],
-[[1,1,0,1,1],[0,0,0,0,0],[1,1,0,1,1],[0,0,0,0,1],[1,1,0,0,0]],
-[[0,0,0,1,1],[0,0,0,1,1],[0,0,0,0,0],[1,1,0,0,0],[1,1,0,0,0]]]
+NIVEL = [
+	[[1,1,0,1,1],[1,0,1,0,1],[0,1,1,1,0],[1,0,1,0,1],[1,1,0,1,1]],
+	[[0,1,0,1,0],[1,1,0,1,1],[0,1,0,1,0],[1,0,1,0,1],[1,0,1,0,1]],
+	[[1,0,0,0,1],[1,1,0,1,1],[0,0,1,0,0],[1,0,1,0,0],[1,0,1,1,0]],
+	[[1,1,0,1,1],[0,0,0,0,0],[1,1,0,1,1],[0,0,0,0,1],[1,1,0,0,0]],
+	[[0,0,0,1,1],[0,0,0,1,1],[0,0,0,0,0],[1,1,0,0,0],[1,1,0,0,0]]
+]
 MAX_TABLERO = 10
 MIN_TABLERO = 5
 ENCENDIDO = 1
@@ -10,37 +12,53 @@ APAGADO = 0
 P_GANAR = 500
 P_PERDER = -300
 P_REINICIAR = -50
+ASCII_NUMBER = 65
+MODO = ["1","2","3"]
+FILA_LETRAS = ["A","B","C","D","E","F","G","H","I","J"]
+MENSAJE_BIENVENIDA = "Bienvenido al juego.\nMENU:\n1- Predeterminado\n2- Aleatorio\n3- Salir "
+MENSAJE_INPUT = "\nIngrese numero segun el modo de juego que quiera jugar.: "
+MENSAJE_REGLAS = """El objetivo del juego es apagar todas las luces del tablero. Los 1 = encenedido y 0= apagado. 
+		Para apagar las luces cuenta con una cantidad limitada de intentos (el triple de las dimensiones del tablero)
+		y se pedira que ingrese las cordenadas del casillero que se desee, y las luces de esas casillas y las cercanas
+		se cambiaran de encendido a apagado o viceversa. Se podra reiniciar el juego en cualquier momento de la partida
+		y se tendra en cuenta los puntos al finalizar."""
+MENSAJE_FIN = "Fin del juego"
+MENSAJE_FILA = "Ingrese un numero de fila o R para reiniciar: "
+MENSAJE_COLUMNA = "Ingrese columna: "
+MODO_PREDETERMINADO = "1"
+MODO_ALEATORIO = "2"
 import random
 import copy
 
+def elegir_modo():
+	print(MENSAJE_BIENVENIDA)
+	modo_juego = "0"
+	while not modo_juego in MODO:
+		modo_juego = input(MENSAJE_INPUT)
+	return modo_juego
+
 def inicio_juego():
 	'''Imprime mensaje de bienvenida y pide modo de juego o salir'''
-	modo = ["1","2","3"]
-	print("Bienvenido al juego.\nMENU:\n1- Predeterminado\n2- Aleatorio\n3- Salir ")
-	modo_juego = input("\nIngrese numero segun el modo de juego que quiera jugar.: ")
-	while not modo_juego in modo:
-		modo_juego = input("Ingrese una opcion valida: ")
-		if modo_juego in modo:
-			break
-	if modo_juego == "1":
+	modo_juego = elegir_modo()
+	if modo_juego == MODO_PREDETERMINADO:
 		reglas(modo_juego)
 		juego_predeterminado()
-	elif modo_juego == "2":
+	elif modo_juego == MODO_ALEATORIO:
 		reglas(modo_juego)
 		juego_aleatorio()
 	else:
-		print("Fin del juego")
+		print(MENSAJE_FIN)
 
 def reglas(modo_juego):
 	'''Imprime las reglas del juego'''
-	if modo_juego == "1" or modo_juego == "2":
-		print("El objetivo del juego es apagar todas las luces del tablero. Los 1 = encenedido y 0= apagado. Para apagar las luces cuenta con una cantidad limitada de intentos (el triple de las dimensiones del tablero) y se pedira que ingrese las cordenadas del casillero que se desee, y las luces de esas casillas y las cercanas se cambiaran de encendido a apagado o viceversa. Se podra reiniciar el juego en cualquier momento de la partida y se tendra en cuenta los puntos al finalizar.")
+	if modo_juego == MODO_PREDETERMINADO or modo_juego == MODO_ALEATORIO:
+		print(MENSAJE_REGLAS)
 
 def elige_fila(tablero):
 	'''pide n? fila o r para reiniciar y seguira preguntando hasta validar condicion, devuelve el fila'''
-	fila = input("Ingrese un numero de fila o R para reiniciar: ")
+	fila = input(MENSAJE_FILA)
 	while ((not fila.isdigit()) or int(fila) > len(tablero) or int(fila) <= 0) and fila.upper() != "R":
-		fila = input("Ingrese n? fila nuevamente o R para reiniciar: ")
+		fila = input(MENSAJE_FILA)
 	if fila.isdigit() and 0 < int(fila) <= len(tablero):
 		fila = int(fila)
 	else:
@@ -51,15 +69,14 @@ def elige_columna(tablero):
 	'''pide columna y hace cambio de letra a n?. sigue preguntando hasta que el n? es del rango del tablero y lo devuelve'''
 	columna = ""
 	while not columna.isalpha() or not len(columna) == 1 or not 0 <= ord(columna.upper()) - 65 < len(tablero):
-		columna = input("Ingrese columna nuevamente: ")
-		if len(columna) == 1 and 0 <= ord(columna.upper()) - 65 < len(tablero):
+		columna = input(MENSAJE_COLUMNA)
+		if len(columna) == 1 and 0 <= ord(columna.upper()) - ASCII_NUMBER < len(tablero):
 			break
-	return ord(columna.upper()) - 65
+	return ord(columna.upper()) - ASCII_NUMBER
 	
 def imprimir_tablero(tablero):
 	'''imprime tablero'''
-	fila_letras = ["A","B","C","D","E","F","G","H","I","J"]
-	print("   ","   ".join(fila_letras[:len(tablero)]),"\n")
+	print("   ","   ".join(FILA_LETRAS[:len(tablero)]),"\n")
 	for i, elem in enumerate(tablero,1):
 		elem = "   ".join(str(num) for num in elem)
 		print(i," ", elem,"\n")
@@ -123,7 +140,7 @@ def juego_aleatorio():
 			columna = elige_columna(tablero)
 			tablero = apagar_prender(tablero,fila,columna)
 			if not hay_luces_encendidas(tablero):
-					break
+				break
 		else:
 			puntos += P_REINICIAR
 			tablero = tablero_aleatorio()
